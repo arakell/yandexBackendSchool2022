0c64b2f0544d7e087ed1ab853190a2bebee99152
arakell <arina.safronova@inbox.ru>
2023-03-28 17:03:09 +0700

    First commit

---
+++ src/main/java/yandexBackendSchool2022/YandexBackendSchool2022Application.java
@@ -0,0 +1,13 @@
+package yandexBackendSchool2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexBackendSchool2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YandexBackendSchool2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/yandexBackendSchool2022/YandexBackendSchool2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package yandexBackendSchool2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexBackendSchool2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
